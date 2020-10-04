1446829d2671e516a4fb951e5ab8250c8b115014
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-10-04 16:42:40 +0200

    Another styling changes in Weather app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5ee26661f85b50fb83ff9c009a1f5ecf&query=' + encodeURI(latitude).replace(',', '.') + ',' + encodeURI(longitude).replace(',', '.') + '&units=m'
-    console.log(url)
     request({ url, json: true }, (err, { body }) => {
         if (err) {
             callback('Unable to connect to weather app', undefined)
