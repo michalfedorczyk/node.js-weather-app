e1ce331d72a5551c558d453a7518124be8ddd0ed
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-10-04 16:33:43 +0200

    Added new information for forecast about pressure

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5ee26661f85b50fb83ff9c009a1f5ecf&query=' + encodeURI(latitude).replace(',', '.') + ',' + encodeURI(longitude).replace(',', '.') + '&units=m'
-
+    console.log(url)
     request({ url, json: true }, (err, { body }) => {
         if (err) {
             callback('Unable to connect to weather app', undefined)
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             console.log('Unable to find location', undefined)
         } else {
             callback(undefined,
-                ('The temperature is: ' + body.current.temperature + ' but it feels like: ' + body.current.feelslike))
+                ('The temperature is: ' + body.current.temperature + ' but it feels like: ' + body.current.feelslike + '. The pressure is: ' + body.current.pressure))
         }
     })
 }
